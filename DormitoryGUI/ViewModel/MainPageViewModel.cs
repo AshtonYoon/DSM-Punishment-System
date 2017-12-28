c69a2dc9730de64850b1e64a25641d60fbfbc573
DSMdongly <gdh0608@naver.com>
2017-12-28 14:16:20 +0900

    edit studentlist viewmodel and request logic

--- DormitoryGUI/ViewModel/MainPageViewModel.cs
+++ DormitoryGUI/ViewModel/MainPageViewModel.cs
@@ -12,11 +12,16 @@ namespace DormitoryGUI.ViewModel
         private bool canEditStudent, canEditScore;
         private string name;
         private int teacherUUID;
+        private string accessToken;
+        private string refreshToken;
         #endregion
 
         #region Properties
-
         public int TeacherUUID { get => teacherUUID; set => teacherUUID = value; }
+
+        public string AccessToken { get => accessToken; set => accessToken = value; }
+
+        public string RefreshToken { get => refreshToken; set => refreshToken = value; }
 
         public string Name { get => name; set => name = value; }
 
